76fe1ce748fb9498e0fca06b860c17008f08ecf2
LarzyyyXD <maksik1383@gmail.com>
2024-11-07 20:34:32 +0300

    Add files via upload

---
+++ folder/shopping_list.py
@@ -0,0 +1,26 @@
+
+
+shopping_list = {}
+
+def add_item(item, quantity=0):
+    if item in shopping_list:
+        shopping_list[item] += quantity
+    else:
+        shopping_list[item] = quantity
+
+def remove_item(item):
+    if item in shopping_list:
+        del shopping_list[item]
+    else:
+        print(f"Товар '{item}' не найден в списке.")
+
+def edit_quantity(item, quantity):
+    if item in shopping_list:
+        shopping_list[item] = quantity
+    else:
+        print(f"Товар '{item}' не найден в списке.")
+
+def view_list():
+    return shopping_list
+
+
